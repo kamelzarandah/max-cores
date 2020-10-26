caab3f290eed59831f536845f0561c7585285c55
kamelzarandah <62485293+kamelzarandah@users.noreply.github.com>
2020-10-26 09:22:14 -0700

    Add files via upload

---
+++ syn_max/main.lua
@@ -0,0 +1,7 @@
+
+Citizen.CreateThread(function()
+    Citizen.InvokeNative("0xCB5D11F9508A928D", 1, a2, a3, -1845241476, 1084182731, 10, 752097756)
+    Citizen.InvokeNative("0xCB5D11F9508A928D", 1, a2, a3, GetHashKey("UPGRADE_STAMINA_TANK_1"), 1084182731, 10, 752097756)
+end)
+
+
