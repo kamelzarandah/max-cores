614b7a14b3a036ee38ee37d1e0252c02389826da
kamelzarandah <62485293+kamelzarandah@users.noreply.github.com>
2020-10-26 11:07:17 -0700

    Update main.lua

--- syn_max/main.lua
+++ syn_max/main.lua
@@ -1,7 +1,13 @@
-
 Citizen.CreateThread(function()
     Citizen.InvokeNative("0xCB5D11F9508A928D", 1, a2, a3, -1845241476, 1084182731, 10, 752097756)
-    Citizen.InvokeNative("0xCB5D11F9508A928D", 1, a2, a3, GetHashKey("UPGRADE_STAMINA_TANK_1"), 1084182731, 10, 752097756)
+end)
+
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        Citizen.InvokeNative("0xCB5D11F9508A928D", 1, a2, a3, GetHashKey("UPGRADE_STAMINA_TANK_1"), 1084182731, 10, 752097756)
+    end
 end)
 
 
